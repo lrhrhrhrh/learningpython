d9b4888210e70cd9fdb4ae249067bb0bd6b44ad9
Chenghao Li <lrhrh@hotmail.com>
2017-07-24 11:11:47 +0800

    添加了序号

--- looplearning.py
+++ looplearning.py
@@ -1,3 +1,10 @@
 languages = ['C','C++','Perl','Python']
 for x in languages:
     print(x)
+
+for i in range(5,9):
+    print(i)
+
+a = ['google', 'baidu', 'taobao', 'facebook']
+for i in range(len(a)):
+    print(i, a[i])
